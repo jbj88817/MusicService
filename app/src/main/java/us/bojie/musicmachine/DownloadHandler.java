5e7dd3996aa9ceac9dd777ab92dcc0d6c46285e9
Bojie Jiang <jiangbojie@gmail.com>
2016-04-06 23:18:17 -0700

    Service setup

--- app/src/main/java/us/bojie/musicmachine/DownloadHandler.java
+++ app/src/main/java/us/bojie/musicmachine/DownloadHandler.java
@@ -10,14 +10,17 @@ import android.util.Log;
 public class DownloadHandler extends Handler {
 
     private static final String TAG = DownloadHandler.class.getSimpleName();
+    private DownloadService mService;
 
     @Override
     public void handleMessage(Message msg) {
-        downloadSong(msg.obj.toString());
+        String message = msg.obj.toString();
+        downloadSong(message);
+        mService.stopSelf(msg.arg1);
     }
 
     private void downloadSong(String song) {
-        long endTime = System.currentTimeMillis() + 10 * 1000;
+        long endTime = System.currentTimeMillis() + 5 * 1000;
         while (System.currentTimeMillis() < endTime) {
             try {
                 Thread.sleep(1000);
@@ -26,5 +29,9 @@ public class DownloadHandler extends Handler {
             }
         }
         Log.d(TAG, song + " downloaded!");
+    }
+
+    public void setService(DownloadService service) {
+        mService = service;
     }
 }
--- app/src/main/java/us/bojie/musicmachine/MainActivity.java
+++ app/src/main/java/us/bojie/musicmachine/MainActivity.java
@@ -1,7 +1,7 @@
 package us.bojie.musicmachine;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_SONG = "song";
     private Button mDownloadButton;
 
     @Override
@@ -17,13 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final DownloadThread thread = new DownloadThread();
-        thread.setName("DownloadThread");
-        thread.start();
-
         mDownloadButton = (Button) findViewById(R.id.download_button);
 
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
@@ -31,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
                 // Send message to handler for processing
                 for (String song : Playlist.songs) {
-                    Message message = Message.obtain();
-                    message.obj = song;
-                    thread.mHandler.sendMessage(message);
+                    Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
                 }
             }
         });
