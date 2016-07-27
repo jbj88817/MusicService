b76d0e1b41dac52eb84fec1e1a2d246c09341d70
Bojie Jiang <jiangbojie@gmail.com>
2016-07-27 15:00:01 -0700

    Handling an Intent from a Filter

--- app/src/main/java/us/bojie/musicmachine/DetailActivity.java
+++ app/src/main/java/us/bojie/musicmachine/DetailActivity.java
@@ -2,11 +2,13 @@ package us.bojie.musicmachine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import us.bojie.musicmachine.models.Song;
@@ -14,51 +16,66 @@ import us.bojie.musicmachine.models.Song;
 /**
  * Created by bjiang on 7/26/16.
  */
-public class DetailActivity extends AppCompatActivity{
+public class DetailActivity extends AppCompatActivity {
 
 
     private Song mSong;
+    private RelativeLayout mRootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        TextView titleLabel = (TextView)findViewById(R.id.songTitleLabel);
-        final CheckBox favoriteCheckbox = (CheckBox)findViewById(R.id.checkBox);
+        TextView titleLabel = (TextView) findViewById(R.id.songTitleLabel);
+        final CheckBox favoriteCheckbox = (CheckBox) findViewById(R.id.checkBox);
+        mRootLayout = (RelativeLayout) findViewById(R.id.rootLayout);
 
         Intent intent = getIntent();
-//        if (intent.getStringExtra(MainActivity.EXTRA_TITLE) != null) {
+
+        if (intent.getAction().equals(Intent.ACTION_SEND)) {
+            handleSendIntent(intent);
+        } else {
+            //        if (intent.getStringExtra(MainActivity.EXTRA_TITLE) != null) {
 //            String songTitle = intent.getStringExtra(MainActivity.EXTRA_TITLE);
 //            if (titleLabel != null) {
 //                titleLabel.setText(songTitle);
 //            }
 //        }
 
-        if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
-            Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
-            if (titleLabel != null) {
-                titleLabel.setText(song.getTitle());
-            }
-            favoriteCheckbox.setChecked(song.isFavorite());
-        }
-
-        final int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
-
-
-        if (favoriteCheckbox != null) {
-            favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    Intent resultIntent = new Intent();
-                    resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
-                    resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
-                    setResult(RESULT_OK, resultIntent);
-                    finish();
+            if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
+                Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
+                if (titleLabel != null) {
+                    titleLabel.setText(song.getTitle());
                 }
-            });
+                favoriteCheckbox.setChecked(song.isFavorite());
+            }
+
+            final int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
+
+
+            if (favoriteCheckbox != null) {
+                favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        Intent resultIntent = new Intent();
+                        resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
+                        resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
+                        setResult(RESULT_OK, resultIntent);
+                        finish();
+                    }
+                });
+            }
         }
     }
+
+    private void handleSendIntent(Intent intent) {
+        if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
+            Snackbar.make(mRootLayout, intent.getStringExtra(Intent.EXTRA_TEXT),
+                    Snackbar.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
